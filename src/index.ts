dc7c02ca02065b54ab5fe663cefc1780305bd92e
Todd Raharivelo <t.raharivelo@outlook.fr>
2023-03-26 00:23:21 +0100

    feat: refacto DOM manipulation

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,20 @@
 import { sum } from './calculator';
 
 window.onload = function() {
+    const firstNumber = document.getElementById("firstNumber");
+    const secondNumber = document.getElementById("secondNumber");
     const resultDiv = document.getElementById("result");
-    console.log(resultDiv.textContent);
+    const button = document.getElementById("button");
+
+    // TODO check input content on first load
+    if (firstNumber.textContent !== "" && firstNumber.textContent !== "") {
+        (button as HTMLInputElement).disabled = false;
+    }
+
+    // TODO check input content on focus / on blur
+
+    console.log("first : ", firstNumber.textContent);
+    console.log("second : ", secondNumber.textContent);
+
     resultDiv.textContent = sum(1,2).toString();
 }
