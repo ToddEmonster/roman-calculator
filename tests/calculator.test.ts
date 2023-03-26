f5b0f0551abaddc05d0197814b11273a4499ad42
Todd Raharivelo <t.raharivelo@outlook.fr>
2023-03-26 15:45:32 +0200

    feat: enum, TODO tests and translator

--- tests/calculator.test.ts
+++ tests/calculator.test.ts
@@ -1,5 +1,16 @@
-import { sum } from '../src/calculator';
+import { sum } from '../src/sum';
 
+// TODELETE
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1,2)).toBe(3);
 })
+
+/*  TODO Is valid roman number :
+- Must be string
+- Must be min 1 char
+- All char must be one of 7 sympols (regex)
+ */
+
+/* TODO Test good translation roman -> arabic */
+
+/* TODO Test good translation arabic -> roman */
