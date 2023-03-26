afcf11e162049518f0e8b6884f842607468be9b9
Todd Raharivelo <t.raharivelo@outlook.fr>
2023-03-26 20:31:16 +0200

    feat: finished roman to arabic converter (TOTEST)

--- src/roman-translator.ts
+++ src/roman-translator.ts
@@ -1,4 +1,5 @@
 import { InvalidRomanNumberError } from './errors/InvalidRomanNumber';
+
 /* 
 There are some rules to a Roman number:
 
@@ -8,8 +9,54 @@ There are some rules to a Roman number:
 + If the numeral is V, L or D you can’t have more than one (“D” + “D” = “M”)
 */
 
-// TODO must throw error
+/**
+ * Couvre aussi le cas "chaîne de caractères vide"
+ */
+const validRomanSymbolsRegex = new RegExp("^[IVXLCDM]+$", "i");
+const invalidMoreThanThreeRegex = new RegExp("(?:I{4,}|X{4,}|C{4,})"); // TOCHECK
+const invalidMoreThanOneRegex = new RegExp("(?:V{2,}|L{2,}|D{2,})"); // TOCHECK
+
+
+/**
+ * TODOC
+ * @param romanNumber a Roman number
+ */
+export function isValidRomanNumber(romanNumber: string): boolean {
+    if (!validRomanSymbolsRegex.test(romanNumber) || 
+        invalidMoreThanThreeRegex.test(romanNumber) || 
+        invalidMoreThanOneRegex.test(romanNumber) )
+        return false;
+
+    return true;
+}
+
+export function isSmallerThan(symbol1: string, symbol2: string) : boolean {
+    return RomanSymbol[symbol1] < RomanSymbol[symbol2];
+}
+
 export function convertRomanNumberToArabic(romanNumber: string): number {
-    throw new InvalidRomanNumberError(romanNumber);
-    //return 0;
+    // try catch
+    if (!isValidRomanNumber(romanNumber)) {
+        throw new InvalidRomanNumberError(romanNumber);
+    }
+
+    let result = 0;
+    for (let i=0; i < romanNumber.length; i++) {
+        const currentSymbol = romanNumber[i];
+        const nextSymbol = romanNumber[i+1];
+
+        // Group with digit smaller than the next one means substraction
+        result += isSmallerThan(currentSymbol,nextSymbol) 
+            ? -RomanSymbol[currentSymbol]
+            : RomanSymbol[currentSymbol];
+    }
+
+    return result;
+}
+
+// TODO ArabicToRoman
+export function convertArabicNumberToRoman(arabicNumber: number): string {
+
+    
+    return "";
 }
