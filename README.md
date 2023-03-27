b519fa9398abe5db314e619a9667196473dc16ca
Todd Raharivelo <t.raharivelo@outlook.fr>
2023-03-27 22:02:05 +0200

    feat(WIP): add SASS files

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # roman-calculator
 
 A kata from https://codingdojo.org/kata/RomanCalculator/
+
+
+
+Source : 
+
+- Webpack config file : ["Webpack in 2023: TypeScript + Jest + Sass + ESLint" by Volodymyr Golosay](https://javascript.plainenglish.io/webpack-in-2021-typescript-jest-sass-eslint-7b4640842e27)
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,54 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: 'style.css',
+      chunkFilename: '[id].css'
+    }),
+  ],
   mode: "development",
   devtool: "inline-source-map",
-  entry: {
-    main: "./src/index.ts",
-  },
+  entry: [
+    __dirname + '/src/index.ts',
+    __dirname + '/src/scss/reset.scss',
+    __dirname + '/src/scss/base.scss',
+    __dirname + '/src/scss/style.scss'
+  ],
   output: {
-    path: path.resolve(__dirname, './public'),
     filename: "[name]-bundle.js", // <--- Will be compiled to this single file
+    path: path.resolve(__dirname, './public'),
     publicPath: '/'
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"],
   },
   module: {
     rules: [
       { 
         test: /\.tsx?$/,
+        exclude: /node_modules/,
         loader: "ts-loader"
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          process.env.NODE_ENV !== 'production'
+            ? 'style-loader'
+            : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
       }
-    ]
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
   },
   devServer: {
     static: {
