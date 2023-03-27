ec10c2935664a99d84baf6f06a750760ac8c213d
Todd Raharivelo <t.raharivelo@outlook.fr>
2023-03-27 19:57:25 +0200

    feat: add dynamic validation errors

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,48 @@
 import { isValidRomanNumber } from './utils';
 
-function eventHandler(event) {
-    if (event.type === "fullscreenchange") {
-      /* handle a full screen toggle */
-    } else {
-      /* handle a full screen toggle error */
-    }
-  }
+/**
+ * Check if the input value is a valid roman number and return a boolean. 
+ * Displays an error message if it's invalid
+ * @param inputDomElement   The input DOM Element 
+ * @param position          The position of the input in the list of all inputs (starting at 1)
+ */
+function isInputValid(inputDomElement: HTMLInputElement, position: number = 1): boolean {
+  if (!isValidRomanNumber(inputDomElement.value)) {
+    // Check if there is already an error message
+    if (document.getElementsByClassName("error number-"+position).length == 0) {
 
-  
+    // Display an error message below the input
+    const numberErrorContainer = document.createElement("p");
+    const numberErrorContent = document.createTextNode("This is not a valid Roman Number");
+    numberErrorContainer.classList.add('error', 'number-'+position);
+    numberErrorContainer.appendChild(numberErrorContent);
+
+    inputDomElement.after(numberErrorContainer);
+    }
+    return false;
+  } else {
+    // Remove error message
+    const errorContainer = document.getElementsByClassName("error number-"+position)[0];
+    if (typeof errorContainer !== "undefined") errorContainer.parentNode.removeChild(errorContainer);
+
+    return true;
+  }
+}
+
 window.onload = function() {
+
     // Get DOM Elements
     const firstNumberDom = document.getElementById("firstNumber") as HTMLInputElement;
     const secondNumberDom = document.getElementById("secondNumber") as HTMLInputElement;
     const resultDiv = document.getElementById("result");
     const button = document.getElementById("button")  as HTMLInputElement;
+
+    // TODO Create DOM Elements (for errors)  
+    const firstNumberErrorDiv = document.createElement("div");
+    const secondNumberErrorDiv = document.createElement("div");
+    const numberErrorContent = document.createTextNode("This is not a valid Roman Number");
+    firstNumberErrorDiv.appendChild(numberErrorContent);
+    secondNumberErrorDiv.appendChild(numberErrorContent);
 
     // Declare values variables
     let firstNumberRoman: string;
@@ -30,20 +58,21 @@ window.onload = function() {
     resultDiv.textContent = "";
     button.disabled = true;
 
-    // TODO Upgrade into clean validation
-    if (!isValidRomanNumber(firstNumberDom.value) && 
-        !isValidRomanNumber(secondNumberDom.value) ) {
-        button.disabled = false;
-    }
-
     // Input changes listeners
     firstNumberDom.addEventListener("input", () => {
-        firstNumberRoman = firstNumberDom.value;
-        console.log(firstNumberRoman);
+      if (!isInputValid(firstNumberDom, 1)) {
+        button.disabled = true;
+      } else {
+        button.disabled = false;
+      }
     });
+
     secondNumberDom.addEventListener("input", () => {
-        secondNumberRoman = secondNumberDom.value;
-        console.log(secondNumberRoman);
+        if (!isInputValid(secondNumberDom, 2)) {
+          button.disabled = true;
+        } else {
+          button.disabled = false;
+        }
     });
 
 
