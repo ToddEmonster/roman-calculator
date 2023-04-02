525fb70ab78ed9d1a97cfe795029be05b80a204a
Todd Raharivelo <t.raharivelo@outlook.fr>
2023-04-02 18:32:11 +0200

    fix: empty handling, paste handling, remove NaN

--- src/index.ts
+++ src/index.ts
@@ -55,36 +55,46 @@ window.onload = function() {
     button.disabled = true;
 
     // Input changes listeners
-    firstNumberDom.addEventListener("input", () => {
-      if (!isValidRomanNumber(firstNumberDom.value)) {
-        displayErrorMessage(firstNumberDom, 1)
-        firstNumberArabic = NaN;
-        firstNumberArabicDom.textContent = "";
-        resultDom.textContent = "";
-        resultArabicDom.textContent = "";
-        button.disabled = true;
-      } else {
-        removeErrorMessage(1);
-        firstNumberArabic = convertRomanNumberToArabic(firstNumberDom.value);
-        firstNumberArabicDom.textContent = firstNumberArabic.toString();
-        button.disabled = !isValidRomanNumber(secondNumberDom.value);
-      }
-    });
-
-    secondNumberDom.addEventListener("input", () => {
-        if (!isValidRomanNumber(secondNumberDom.value)) {
-          displayErrorMessage(secondNumberDom, 2)
-          secondNumberArabic = NaN;
-          secondNumberArabicDom.textContent = "";
-          resultDom.textContent = "";
-          resultArabicDom.textContent = "";
-          button.disabled = true;
+    ['input', 'onpaste'].forEach( (event) => {
+      firstNumberDom.addEventListener(event, () => {
+        if (firstNumberDom.value.length > 0) {
+          if (!isValidRomanNumber(firstNumberDom.value)) {
+            displayErrorMessage(firstNumberDom, 1)
+            firstNumberArabicDom.textContent = "";
+            resultDom.textContent = "";
+            resultArabicDom.textContent = "";
+            button.disabled = true;
+          } else {
+            removeErrorMessage(1);
+            firstNumberArabic = convertRomanNumberToArabic(firstNumberDom.value);
+            firstNumberArabicDom.textContent = firstNumberArabic.toString();
+            button.disabled = !isValidRomanNumber(secondNumberDom.value);
+          }
         } else {
-          removeErrorMessage(2);
-          secondNumberArabic = convertRomanNumberToArabic(secondNumberDom.value);
-          secondNumberArabicDom.textContent = secondNumberArabic.toString();
-          button.disabled = !isValidRomanNumber(firstNumberDom.value);
+          firstNumberArabicDom.textContent = "";
+          removeErrorMessage(1);
         }
+      });
+
+      secondNumberDom.addEventListener(event, () => {
+        if (secondNumberDom.value.length > 0) {
+          if (!isValidRomanNumber(secondNumberDom.value)) {
+            displayErrorMessage(secondNumberDom, 2)
+            secondNumberArabicDom.textContent = "";
+            resultDom.textContent = "";
+            resultArabicDom.textContent = "";
+            button.disabled = true;
+          } else {
+            removeErrorMessage(2);
+            secondNumberArabic = convertRomanNumberToArabic(secondNumberDom.value);
+            secondNumberArabicDom.textContent = secondNumberArabic.toString();
+            button.disabled = !isValidRomanNumber(firstNumberDom.value);
+          }
+        } else {
+          secondNumberArabicDom.textContent = "";
+          removeErrorMessage(2);
+        }
+      });
     });
 
     // Listen to button click
